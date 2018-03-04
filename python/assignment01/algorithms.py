7d8f2995aee487bfc4fbd949a35a4f1da2329afd
jay ess <mentoce@gmail.com>
2018-03-04 11:34:45 -0800

    Add start of Python code for assignment 1

---
+++ python/assignment01/algorithms.py
@@ -0,0 +1,17 @@
+"""This modules contains different machine learning algorithms such as:
+    - minimum square error
+    - gradient descent
+    - steepest descent
+"""
+import numpy as np
+
+def mean_square_error():
+    pass
+
+
+def gradient_descent():
+    pass
+
+
+def steepest_descent():
+    pass
